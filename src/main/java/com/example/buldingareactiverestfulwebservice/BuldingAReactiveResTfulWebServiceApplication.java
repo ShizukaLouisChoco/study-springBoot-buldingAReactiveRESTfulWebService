1cb67fb610f3f42ad57c6c32c030ebe97daf8e75
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-03 15:51:27 +0100

    first commit

---
+++ src/main/java/com/example/buldingareactiverestfulwebservice/BuldingAReactiveResTfulWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.buldingareactiverestfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuldingAReactiveResTfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuldingAReactiveResTfulWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/buldingareactiverestfulwebservice/BuldingAReactiveResTfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buldingareactiverestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuldingAReactiveResTfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
