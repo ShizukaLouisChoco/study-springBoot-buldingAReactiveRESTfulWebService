c95c1bf10c615904458428ec06482b97f86ad083
Shizuka Tanigawa <chocokkuma@gmail.com>
2022-12-03 17:43:38 +0100

    4) Make the Application Executable

--- src/main/java/com/example/buldingareactiverestfulwebservice/BuldingAReactiveResTfulWebServiceApplication.java
+++ src/main/java/com/example/buldingareactiverestfulwebservice/BuldingAReactiveResTfulWebServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.buldingareactiverestfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BuldingAReactiveResTfulWebServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BuldingAReactiveResTfulWebServiceApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(BuldingAReactiveResTfulWebServiceApplication.class, args);
+        GreetingClient greetingClient = context.getBean(GreetingClient.class);
+        // We need to block for the content here or the JVM might exit before the message is logged
+        System.out.println(">> message = " + greetingClient.getMessage().block());
     }
-
 }
+
